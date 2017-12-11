c6833fb69e7c15431e0d13cd56de5c620c8604d0
fededitullio <feder.ditullio@gmail.com>
2017-12-11 12:29:41 +0100

    Users models

--- src/main/java/it/dstech/models/User.java
+++ src/main/java/it/dstech/models/User.java
@@ -1,5 +1,127 @@
 package it.dstech.models;
 
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String telefono;
+	
+	private String via;
+	
+	private String citta;
+	
+	private String provincia;
+	@JsonIgnore
+	@OneToMany
+	@JoinColumn(name="User_id")
+	private List<CreditCard> listCard;
+	
+	private List<Product> listProduct;
+	
+	public UserProfileType getProfileType() {
+		return profileType;
+	}
+
+	public void setProfileType(UserProfileType profileType) {
+		this.profileType = profileType;
+	}
+
+	private UserProfileType profileType;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getVia() {
+		return via;
+	}
+
+	public void setVia(String via) {
+		this.via = via;
+	}
+
+	public String getCitta() {
+		return citta;
+	}
+
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public List<CreditCard> getListCard() {
+		return listCard;
+	}
+
+	public void setListCard(List<CreditCard> listCard) {
+		this.listCard = listCard;
+	}
+
+	public List<Product> getListProduct() {
+		return listProduct;
+	}
+
+	public void setListProduct(List<Product> listProduct) {
+		this.listProduct = listProduct;
+	}
+
+	public User() {
+		this.listProduct = new ArrayList<Product>();
+		this.listCard = new ArrayList<CreditCard>();
+		super();
+	}
+	
 	
 }
---
+++ src/main/java/it/dstech/models/UserProfileType.java
@@ -0,0 +1,5 @@
+package it.dstech.models;
+
+public enum UserProfileType {
+ROLE_USER, ROLE_ADMIN
+}
